98f9646b23e89664e2d95edd2efcc442b0d38bdc
gotuzzoj23 <gotuzzoj23@gmail.com>
2016-03-12 22:50:11 -0800

    assignment 3

---
+++ solution--gotuzzoj23/main.cpp
@@ -0,0 +1,90 @@
+//
+//  main.cpp
+//  assig3
+//
+//  Created by Jose Gotuzzo, Robert Gonzalez, Griffin Yee on 3/12/16.
+//  Copyright © 2016 Jose Gotuzzo. All rights reserved.
+//
+
+
+#include <iostream>
+#include <string>
+#include "part123.h"
+#include "part45.h"
+using std::cout;
+using std::cin;
+using std::string;
+using std:: endl;
+
+
+int main() {
+//  division (); //part 1 divide any two numbers, but returns error when diving by 0.
+  
+/*
+    int x;
+    double y;
+    cout << "Please enter an integer and a deicmal number: " << endl;
+    cin >> x >> y;
+    cout<< abs_value(x) << endl; //part 2 finding absolute value of a integer with template function
+    cout<< abs_value(y) << endl; //part 2 finding absolute value of a decimal with template
+*/
+
+//  ranking(); //part 3 Using vectors to let user enter a score and rank it in a list.
+   
+/*
+    int c, d;
+    cout<< "Enter two numbers to find there greatest common divisor." << endl;
+    cin >> c >> d;              // euclids algorithm
+    cout << gcd(c,d)<< endl;
+*/
+    
+/*
+    int n;
+    cout << "Enter a number to find the Fibonacci sequence to. ";   //fibonacci sequence
+    cin >> n;
+    cout << fib(n) << endl;
+*/
+    
+/*
+    int a , b;
+    cout << "Enter a number and an exponent to find the power to it." <<endl;//power fucntion
+    cin >> a >> b;
+    cout<<pow(a,b)<<endl;
+*/
+
+/*
+    int a;
+    cout << "Preferred Triangle size: ";
+    cin >> a;                               /////nth triangle 
+    cout << tri(a) << endl;
+    display (a,tri(a));
+*/
+
+/*
+     int x, y;
+     cout<< "Enter two numbers to find there greatest common divisor." << endl;
+     cin >> x >> y;              // euclids algorithm
+     cout << gcd_iter(x,y)<< endl;
+*/
+
+/*
+    int x;
+    cout << "Enter a number to find the Fibonacci sequence to. ";   //fibonacci sequence
+    cin >> x;
+    cout << fib_iter(x) << endl;
+*/
+
+/*
+    int x, y;
+    cout << "Enter a number and an exponent to find the power to it." <<endl;//power fucntion
+    cin >> x >> y;
+    cout << pow_iter(x , y) << endl;
+*/
+
+    return 0;
+}
+
+
+     
+
+
